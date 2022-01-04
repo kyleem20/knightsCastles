4811825ffba92dbae93b6db4fae96d88e882e282
Kylee <kylee.j.marlin@gmail.com>
2022-01-04 16:24:03 -0700

    update

--- knightsCastles/dbSetup.sql
+++ knightsCastles/dbSetup.sql
@@ -6,3 +6,21 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS knights(
+  id INT AUTO_INCREMENT NOT NULL primary key,
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  title TEXT NOT NULL COMMENT 'Knight Name',
+  horseId INT,
+  castleId INT,
+  creatorId VARCHAR(255) NOT NULL COMMENT 'The ownwer/creator of the habit',
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS castles(
+  id INT AUTO_INCREMENT NOT NULL primary key,
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  title TEXT NOT NULL COMMENT 'Castle Name',
+  creatorId VARCHAR(255) NOT NULL COMMENT 'The ownwer/creator of the habit',
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
